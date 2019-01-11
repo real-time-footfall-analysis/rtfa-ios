690ec3ae2ef82305932594046b6742b962867ba5
Jack Chorley <jack.chorley16@imperial.ac.uk>
2019-01-11 13:19:01 +0000

    Big Delete

--- Pods/PushNotifications/Sources/SDK.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-struct SDK {
-    static let version = "1.1.0"
-}
--- Pods/Target Support Files/Pods-rtfa-iosTests/Pods-rtfa-iosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_rtfa_iosTests : NSObject
-@end
-@implementation PodsDummy_Pods_rtfa_iosTests
-@end
